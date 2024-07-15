f850e6f90fe8d3906445f2ab62b70b566f004978
Andy Hubert <andy.hubert@gmail.com>
2024-07-15 20:02:55 +0000

    comments

--- yoseph/main.js
+++ yoseph/main.js
@@ -2,3 +2,13 @@ setTimeout(
     () => alert('woooooo'),
     1000*2
 )
+/*
+money in divide with math function
+a second subtractive function
+make the money displayes into varialbes
+the empty button simply makes t-money display 0
+the subtractive inputs subtract le number (in the form of a veriable) from u/s money display, and recordes?
+history??? but how!? (continued):
+make both money and explenation a single string, then add to array? or...
+add both to object and add object to array?
+or give up!!!!!!!! (definitly this one)
